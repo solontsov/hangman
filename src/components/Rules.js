df2d054242a8ee07f91ceaca5998900a9abbb670
Oleksandr Solontsov <solontsov@gmail.com>
2023-02-19 21:36:48 +0000

    rules

---
+++ src/components/Rules.js
@@ -0,0 +1,14 @@
+//Component that shows how to play Hangman game.
+
+function Rules() {
+    const rules = "Use the mouse to play the game. Try to guess the word hidden. Dashes represent letters of the word. Choose the alphabet letters below. Do your best to guess correctly or the funny stickman gets hanged.";
+
+    return (
+        <div>
+            <h2>How to play Hangman game</h2>
+            <p>{rules}</p>
+        </div>
+    )
+}
+
+export default Rules;
