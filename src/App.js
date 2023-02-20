d7c9c5941087e69cf7cab6a912b6676b3c717c15
Oleksandr Solontsov <solontsov@gmail.com>
2023-02-20 10:33:22 +0000

    words dict-ry

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Word from './components/Word';
 import Alphabet from './components/Alphabet';
 import Rules from './components/Rules';
 
+import { words } from './words';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hangman game</h1>
+        <h1>Hangman game {words[23]}</h1>
         {/* TODO:  add button HELP, RESTART */}
         <Rules showRules={true}/>
         <Word />
